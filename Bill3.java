76abbd29df1178b887ea683797ce9ff3abac3883
Ofri Vaknin <vakninofri@gmail.com>
2024-11-07 19:22:23 +0200

    Finished Bill3

--- Bill3.java
+++ Bill3.java
@@ -1,14 +1,14 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
-	public static void main(String[] args, int price) {
+	public static void main(String[] args) {
 		//Takes the inputs from the user.
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-		double bill = price / 3;
+		double billInput = Double.parseDouble(args[3]);
 		//rounds up the double
-		bill = Math.ceil(bill);
-		System.out.println("Dear " + name1 + ", " + name2 + ", and" + name3 + "pay " + bill + " Shekels each.");
+		billInput = Math.ceil(billInput / 3);
+		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + " pay " + billInput + " Shekels each.");
 	   
 	}
 }
