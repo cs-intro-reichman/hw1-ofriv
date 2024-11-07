e467eb4af1692fb7efd675a2ca1f3a8b93a7c2c8
Ofri Vaknin <vakninofri@gmail.com>
2024-11-07 19:11:51 +0200

    First Test

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,14 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
-	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+	public static void main(String[] args, int price) {
+		//Takes the inputs from the user.
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		double bill = price / 3;
+		//rounds up the double
+		bill = Math.ceil(bill);
+		System.out.println("Dear " + name1 + ", " + name2 + ", and" + name3 + "pay " + bill + " Shekels each.");
+	   
 	}
 }
